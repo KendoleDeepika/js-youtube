a461edcb301e6457978b4a3ee4c6e85671656056
KendoleDeepika <149302537+KendoleDeepika@users.noreply.github.com>
2025-04-18 10:27:11 +0000

    varables

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -9,3 +9,25 @@
 console.log(3+3);
 
 console.log("hitesg");
+
+
+let name = "Deepu"
+let age = 25
+let isLoggedIn = false
+
+//primitive data types
+// number =>2 to power  53
+//bigint
+//string => ""
+//boolean => true/false
+//null => empty value // standard alone value
+//undefined => variable is declared but not assigned
+//symobl => unique value
+
+console.log(typeof "Deepu"); // string
+console.log(typeof null); //object
+// null is a special value in JavaScript that represents the intentional absence of any object value. 
+console.log(typeof undefined); //undefined
+// undefined is a type itself (undefined) and also a value (undefined).
+//object => collection of key value pairs
+
