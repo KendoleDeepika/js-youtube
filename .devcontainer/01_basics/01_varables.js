6546925a3c13521afd6efd533b334b4be48bc213
KendoleDeepika <149302537+KendoleDeepika@users.noreply.github.com>
2025-04-18 10:26:21 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_varables.js
@@ -0,0 +1,6 @@
+
+// Declare a variable
+let myVariable = "Hello, World!";
+
+// Log the variable to the console
+console.log(myVariable);
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+/* Datatypes */
+
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser.
+// alert("Hello World!"); // show alert box
+// alert is a browser-specific function and does not work in Node.js
+
+console.log(3+3);
+
+console.log("hitesg");
